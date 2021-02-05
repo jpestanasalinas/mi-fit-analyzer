4b0102e7f37f4542ede9965d799b976afcc047fb
jpestanasalinas <julio.p.salinas@gmail.com>
2021-02-05 19:17:58 +0100

    creating application

---
+++ src/main/java/com/jpestana/mifitanalyzer/MiFitAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.jpestana.mifitanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiFitAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiFitAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpestana/mifitanalyzer/MiFitAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpestana.mifitanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiFitAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
